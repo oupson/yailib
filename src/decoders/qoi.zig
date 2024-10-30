40767aef91c32acb80eb0bf1d53faaec20a6beaa
oupson <oupson1er@gmail.com>
2024-10-30 13:03:13 +0100

    Don't read all image bytes in memory

--- src/decoders/qoi.zig
+++ src/decoders/qoi.zig
@@ -71,8 +71,8 @@ const QoiDecoder = struct {
     image: ?lib.Image,
     runningArray: RunningArray,
     pixelIndex: usize = 0,
-    cache: [4]u8 = [_]u8{0} ** 4,
-    cache_buffer: []u8 = &.{},
+    cache: [5]u8 = [_]u8{0} ** 5,
+    cacheBuffer: []u8 = &.{},
     latest: Pixel = .{},
 
     const Self = @This();
@@ -91,119 +91,41 @@ const QoiDecoder = struct {
         self.buffer = buffer;
     }
 
+    fn deinit(ptr: *anyopaque) void {
+        const self: *Self = @ptrCast(@alignCast(ptr));
+        _ = self;
+    }
+
+    fn getResult(ptr: *anyopaque) DecoderError!lib.Image {
+        const self: *Self = @ptrCast(@alignCast(ptr));
+        const res = self.image;
+        self.image = null;
+        return res.?;
+    }
+
     fn step(ptr: *anyopaque) DecoderError!Event {
         const self: *Self = @ptrCast(@alignCast(ptr));
-        if (self.image) |img| {
-            var index: usize = 0;
+        var index: usize = if (self.cacheBuffer.len > 0)
+            try self.readFromCache(self.buffer)
+        else
+            0;
 
+        self.cacheBuffer = &.{};
+
+        if (self.image) |img| {
             while (index < self.buffer.len) {
                 if (self.pixelIndex == img.pixels.len) {
                     // TODO: check end
                     return Event.Finished;
-                } else if (self.buffer[index] == 0b11111110) {
-                    const p = Pixel{
-                        .r = self.buffer[index + 1],
-                        .g = self.buffer[index + 2],
-                        .b = self.buffer[index + 3],
-                    };
-
-                    self.runningArray.put(p);
-                    self.latest = p;
-                    self.image.?.pixels[self.pixelIndex] = p.r;
-                    self.image.?.pixels[self.pixelIndex + 1] = p.g;
-                    self.image.?.pixels[self.pixelIndex + 2] = p.b;
-
-                    self.pixelIndex += 3;
-
-                    index += 4;
-                } else if (self.buffer[index] == 0x11111111) {
-                    const p = Pixel{
-                        .r = self.buffer[index + 1],
-                        .g = self.buffer[index + 2],
-                        .b = self.buffer[index + 3],
-                        .a = self.buffer[index + 4],
-                    };
-
-                    self.runningArray.put(p);
-                    self.latest = p;
-                    self.image.?.pixels[self.pixelIndex] = p.r;
-                    self.image.?.pixels[self.pixelIndex + 1] = p.g;
-                    self.image.?.pixels[self.pixelIndex + 2] = p.b;
-                    // TODO: alpha
-
-                    self.pixelIndex += 3;
-
-                    index += 5;
-                } else if (self.buffer[index] & 0b11000000 == 0) {
-                    // HASH
-                    const p = self.runningArray.get(@as(usize, self.buffer[index] & 0b00111111));
-
-                    self.latest = p;
-                    self.runningArray.put(p);
-
-                    self.image.?.pixels[self.pixelIndex] = p.r;
-                    self.image.?.pixels[self.pixelIndex + 1] = p.g;
-                    self.image.?.pixels[self.pixelIndex + 2] = p.b;
-                    self.pixelIndex += 3;
-                    index += 1;
-                } else if (@shrExact(self.buffer[index] & 0b11000000, 6) == 0b01) {
-                    var latest = self.latest;
-                    // QOI_OP_DIFF
-
-                    const diffR = @shrExact(self.buffer[index] & 0b00110000, 4) -% 2;
-                    const diffG = @shrExact(self.buffer[index] & 0b00001100, 2) -% 2;
-                    const diffB = @shrExact(self.buffer[index] & 0b00000011, 0) -% 2;
-
-                    latest.r = latest.r +% diffR;
-                    latest.g = latest.g +% diffG;
-                    latest.b = latest.b +% diffB;
-
-                    self.runningArray.put(latest);
-                    self.latest = latest;
-
-                    self.image.?.pixels[self.pixelIndex] = latest.r;
-                    self.image.?.pixels[self.pixelIndex + 1] = latest.g;
-                    self.image.?.pixels[self.pixelIndex + 2] = latest.b;
-                    // TODO: alpha
-
-                    self.pixelIndex += 3;
-                    index += 1;
-                } else if (@shrExact(self.buffer[index] & 0b11000000, 6) == 0b10) {
-                    var latest = self.latest;
-
-                    const dg = (self.buffer[index] & 0b00111111) -% 32;
-
-                    const dr = (@shrExact(self.buffer[index + 1] & 0b11110000, 4) -% 8) +% dg;
-                    const db = (@shrExact(self.buffer[index + 1] & 0b00001111, 0) -% 8) +% dg;
-
-                    latest.r = latest.r +% dr;
-                    latest.g = latest.g +% dg;
-                    latest.b = latest.b +% db;
-
-                    self.runningArray.put(latest);
-                    self.latest = latest;
-
-                    self.image.?.pixels[self.pixelIndex] = latest.r;
-                    self.image.?.pixels[self.pixelIndex + 1] = latest.g;
-                    self.image.?.pixels[self.pixelIndex + 2] = latest.b; // TODO: alpha
-
-                    self.pixelIndex += 3;
-                    index += 2;
-                } else if (@shrExact(self.buffer[index] & 0b11000000, 6) == 0b11) {
-                    const run = (self.buffer[index] & 0b00111111) + 1;
-
-                    const latest = self.latest;
-                    for (0..run) |_| {
-                        self.image.?.pixels[self.pixelIndex] = latest.r;
-                        self.image.?.pixels[self.pixelIndex + 1] = latest.g;
-                        self.image.?.pixels[self.pixelIndex + 2] = latest.b;
-                        // TODO: alpha
-                        self.pixelIndex += 3;
-                    }
-
-                    index += 1;
                 } else {
-                    return DecoderError.UnknownFormat;
+                    const buffer = self.buffer[index..];
+
+                    const read = try self.readImage(buffer);
+                    if (read == 0) {
+                        return Event.NeedMoreData;
+                    } else {
+                        index += read;
+                    }
                 }
             }
             self.buffer = &.{};
@@ -225,15 +147,160 @@ const QoiDecoder = struct {
         }
     }
 
-    fn deinit(ptr: *anyopaque) void {
-        const self: *Self = @ptrCast(@alignCast(ptr));
-        _ = self;
+    inline fn readFromCache(self: *Self, buffer: []const u8) DecoderError!usize {
+        const cacheLen = self.cacheBuffer.len;
+        {
+            const missingLen = self.cache.len - cacheLen;
+            if (missingLen > buffer.len) {
+                // todo: error
+            } else {
+                std.mem.copyForwards(u8, self.cache[cacheLen..], buffer[0..missingLen]);
+                self.cacheBuffer = &self.cache;
+            }
+        }
+        if (self.cacheBuffer[0] == 0b11111110) {
+            self.readRgb(self.cacheBuffer);
+            return 4 - cacheLen;
+        } else if (self.cacheBuffer[0] == 0x11111111) {
+            self.readRgba(self.cacheBuffer);
+            return 5 - cacheLen;
+        } else if (@shrExact(self.cacheBuffer[0] & 0b11000000, 6) == 0b10) {
+            self.readLuma(self.cacheBuffer);
+            return 2 - cacheLen;
+        }
+        return DecoderError.UnknownFormat;
     }
 
-    fn getResult(ptr: *anyopaque) DecoderError!lib.Image {
-        const self: *Self = @ptrCast(@alignCast(ptr));
-        const res = self.image;
-        self.image = null;
-        return res.?;
+    inline fn readImage(self: *Self, buffer: []const u8) DecoderError!usize {
+        if (buffer[0] == 0b11111110) {
+            if (buffer.len < 4) {
+                self.cacheBuffer = self.cache[0..buffer.len];
+                std.mem.copyForwards(u8, self.cacheBuffer, buffer);
+                return 0;
+            }
+            self.readRgb(buffer);
+            return 4;
+        } else if (buffer[0] == 0x11111111) {
+            if (buffer.len < 5) {
+                self.cacheBuffer = self.cache[0..buffer.len];
+                std.mem.copyForwards(u8, self.cacheBuffer, buffer);
+                return 0;
+            }
+            self.readRgba(buffer);
+            return 5;
+        } else if (buffer[0] & 0b11000000 == 0) {
+            // HASH
+            const p = self.runningArray.get(@as(usize, buffer[0] & 0b00111111));
+
+            self.latest = p;
+            self.runningArray.put(p);
+
+            self.image.?.pixels[self.pixelIndex] = p.r;
+            self.image.?.pixels[self.pixelIndex + 1] = p.g;
+            self.image.?.pixels[self.pixelIndex + 2] = p.b;
+            self.pixelIndex += 3;
+            return 1;
+        } else if (@shrExact(buffer[0] & 0b11000000, 6) == 0b01) {
+            var latest = self.latest;
+            // QOI_OP_DIFF
+
+            const diffR = @shrExact(buffer[0] & 0b00110000, 4) -% 2;
+            const diffG = @shrExact(buffer[0] & 0b00001100, 2) -% 2;
+            const diffB = @shrExact(buffer[0] & 0b00000011, 0) -% 2;
+
+            latest.r = latest.r +% diffR;
+            latest.g = latest.g +% diffG;
+            latest.b = latest.b +% diffB;
+
+            self.runningArray.put(latest);
+            self.latest = latest;
+
+            self.image.?.pixels[self.pixelIndex] = latest.r;
+            self.image.?.pixels[self.pixelIndex + 1] = latest.g;
+            self.image.?.pixels[self.pixelIndex + 2] = latest.b;
+            // TODO: alpha
+
+            self.pixelIndex += 3;
+            return 1;
+        } else if (@shrExact(buffer[0] & 0b11000000, 6) == 0b10) {
+            if (buffer.len < 3) {
+                self.cacheBuffer = self.cache[0..buffer.len];
+                std.mem.copyForwards(u8, self.cacheBuffer, buffer);
+                return 0;
+            }
+            self.readLuma(buffer);
+            return 2;
+        } else if (@shrExact(buffer[0] & 0b11000000, 6) == 0b11) {
+            const run = (buffer[0] & 0b00111111) + 1;
+
+            const latest = self.latest;
+            for (0..run) |_| {
+                self.image.?.pixels[self.pixelIndex] = latest.r;
+                self.image.?.pixels[self.pixelIndex + 1] = latest.g;
+                self.image.?.pixels[self.pixelIndex + 2] = latest.b;
+                // TODO: alpha
+                self.pixelIndex += 3;
+            }
+
+            return 1;
+        } else {
+            return DecoderError.UnknownFormat;
+        }
+    }
+
+    inline fn readRgb(self: *Self, buffer: []const u8) void {
+        const p = Pixel{
+            .r = buffer[1],
+            .g = buffer[2],
+            .b = buffer[3],
+        };
+
+        self.runningArray.put(p);
+        self.latest = p;
+        self.image.?.pixels[self.pixelIndex] = p.r;
+        self.image.?.pixels[self.pixelIndex + 1] = p.g;
+        self.image.?.pixels[self.pixelIndex + 2] = p.b;
+
+        self.pixelIndex += 3;
+    }
+
+    inline fn readRgba(self: *Self, buffer: []const u8) void {
+        const p = Pixel{
+            .r = buffer[1],
+            .g = buffer[2],
+            .b = buffer[3],
+            .a = buffer[4],
+        };
+
+        self.runningArray.put(p);
+        self.latest = p;
+        self.image.?.pixels[self.pixelIndex] = p.r;
+        self.image.?.pixels[self.pixelIndex + 1] = p.g;
+        self.image.?.pixels[self.pixelIndex + 2] = p.b;
+        // TODO: alpha
+
+        self.pixelIndex += 3;
+    }
+
+    inline fn readLuma(self: *Self, buffer: []const u8) void {
+        var latest = self.latest;
+
+        const dg = (buffer[0] & 0b00111111) -% 32;
+
+        const dr = (@shrExact(buffer[1] & 0b11110000, 4) -% 8) +% dg;
+        const db = (@shrExact(buffer[1] & 0b00001111, 0) -% 8) +% dg;
+
+        latest.r = latest.r +% dr;
+        latest.g = latest.g +% dg;
+        latest.b = latest.b +% db;
+
+        self.runningArray.put(latest);
+        self.latest = latest;
+
+        self.image.?.pixels[self.pixelIndex] = latest.r;
+        self.image.?.pixels[self.pixelIndex + 1] = latest.g;
+        self.image.?.pixels[self.pixelIndex + 2] = latest.b; // TODO: alpha
+
+        self.pixelIndex += 3;
     }
 };
